143727355b4eef2b400c16126528c10300e046ea
Luuk van Keeken <luukvankeeken24@gmail.com>
2024-02-16 11:11:28 +0100

    Started working on implementation of neuromodulated time-constants.

--- ncps/torch/cfc_cell.py
+++ ncps/torch/cfc_cell.py
@@ -44,6 +44,8 @@ class CfCCell(nn.Module):
         backbone_layers=1,
         backbone_dropout=0.0,
         sparsity_mask=None,
+        neuromod_network_dims=None,
+        neuromod_network_activation=nn.Tanh,
     ):
         """A `Closed-form Continuous-time <https://arxiv.org/abs/2106.13898>`_ cell.
 
@@ -81,6 +83,7 @@ class CfCCell(nn.Module):
         )
 
         self.mode = mode
+        self.neuromod_network_dims = neuromod_network_dims
 
         if backbone_activation == "silu":
             backbone_activation = nn.SiLU
@@ -122,6 +125,12 @@ class CfCCell(nn.Module):
             self.A = torch.nn.Parameter(
                 data=torch.ones(1, self.hidden_size), requires_grad=True
             )
+        elif self.mode == "neuromodulated":
+            layer_list = []
+            for i in range(len(neuromod_network_dims) - 1):
+                layer_list.append(nn.Linear(neuromod_network_dims[i], neuromod_network_dims[i + 1]))
+                layer_list.append(nn.Linear(neuromod_network_activation))
+            self.neuromod = nn.Sequential(*layer_list)
         else:
             self.ff2 = nn.Linear(cat_shape, hidden_size)
             self.time_a = nn.Linear(cat_shape, hidden_size)
@@ -141,39 +150,42 @@ class CfCCell(nn.Module):
                 torch.nn.init.xavier_uniform_(w)
 
     def forward(self, input, hx, ts):
-        x = torch.cat([input, hx], 1)
-        if self.backbone_layers > 0:
-            x = self.backbone(x)
-        if self.sparsity_mask is not None:
-            ff1 = F.linear(x, self.ff1.weight * self.sparsity_mask, self.ff1.bias)
+        if self.mode == "neuromodulated":
+            assert len()
         else:
-            ff1 = self.ff1(x)
-        if self.mode == "pure":
-            # Solution
-            new_hidden = (
-                -self.A
-                * torch.exp(-ts * (torch.abs(self.w_tau) + torch.abs(ff1)))
-                * ff1
-                + self.A
-            )
-
-            # This calculation of the tau system seems to be in accordance
-            # with equations 1, 2, and 3 in "Closed-form Continuous-time
-            # Neural Networks".
-            self.tau_system.data = 1.0 / (torch.abs(self.w_tau) + torch.abs(ff1))
-        else:
-            # Cfc
+            x = torch.cat([input, hx], 1)
+            if self.backbone_layers > 0:
+                x = self.backbone(x)
             if self.sparsity_mask is not None:
-                ff2 = F.linear(x, self.ff2.weight * self.sparsity_mask, self.ff2.bias)
+                ff1 = F.linear(x, self.ff1.weight * self.sparsity_mask, self.ff1.bias)
             else:
-                ff2 = self.ff2(x)
-            ff1 = self.tanh(ff1)
-            ff2 = self.tanh(ff2)
-            t_a = self.time_a(x)
-            t_b = self.time_b(x)
-            t_interp = self.sigmoid(t_a * ts + t_b)
-            if self.mode == "no_gate":
-                new_hidden = ff1 + t_interp * ff2
+                ff1 = self.ff1(x)
+            if self.mode == "pure":
+                # Solution
+                new_hidden = (
+                    -self.A
+                    * torch.exp(-ts * (torch.abs(self.w_tau) + torch.abs(ff1)))
+                    * ff1
+                    + self.A
+                )
+
+                # This calculation of the tau system seems to be in accordance
+                # with equations 1, 2, and 3 in "Closed-form Continuous-time
+                # Neural Networks".
+                self.tau_system.data = 1.0 / (torch.abs(self.w_tau) + torch.abs(ff1))
             else:
-                new_hidden = ff1 * (1.0 - t_interp) + t_interp * ff2
+                # Cfc
+                if self.sparsity_mask is not None:
+                    ff2 = F.linear(x, self.ff2.weight * self.sparsity_mask, self.ff2.bias)
+                else:
+                    ff2 = self.ff2(x)
+                ff1 = self.tanh(ff1)
+                ff2 = self.tanh(ff2)
+                t_a = self.time_a(x)
+                t_b = self.time_b(x)
+                t_interp = self.sigmoid(t_a * ts + t_b)
+                if self.mode == "no_gate":
+                    new_hidden = ff1 + t_interp * ff2
+                else:
+                    new_hidden = ff1 * (1.0 - t_interp) + t_interp * ff2
         return new_hidden, new_hidden
